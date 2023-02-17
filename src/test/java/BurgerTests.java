21685cf28a8a3641fe34163ce44a2d810834613d
Anastasia Radachinskaia <asya070692@mail.ru>
2023-02-17 01:08:07 +0100

    pom & something

---
+++ src/test/java/BurgerTests.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.*;
+import org.junit.Assert.*;
+import org.junit.runner.*;
+import org.mockito.*;
+import org.mockito.junit.*;
+import praktikum.Burger;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class BurgerTests {
+    @Mock
+    Burger burger;
+
+    @Test
+    public void getSoundReturnMeowTest()
+
+}
